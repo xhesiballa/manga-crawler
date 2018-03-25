56eec5a23c1be05a23f8beb62cdb114977bbeb57
Xhesi Balla <xhesiballa15@gmail.com>
2018-03-26 00:08:17 +0200

    Implement filtering of chapter list by name

--- src/main/java/com/xhesiballa/crawler/App.java
+++ src/main/java/com/xhesiballa/crawler/App.java
@@ -8,11 +8,13 @@ import com.xhesiballa.crawler.ui.components.ClientsTable;
 import com.xhesiballa.crawler.ui.components.MangaTable;
 import javafx.application.Application;
 import javafx.application.Platform;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.*;
 import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
@@ -22,8 +24,9 @@ import java.util.List;
 import java.util.function.Consumer;
 
 public class App extends Application {
-    private static final String SAVE_LOCATION = "C:/Users/user/Desktop/";
+    private static final String SAVE_LOCATION = "C:/Users/xhesi/Desktop/";
     private static final String FILE_EXTENSION = ".jpg";
+    private static final String SEARCH_STRING = "Search";
 
     private static Config config;
     private static ClientFactory clientFactory;
@@ -32,13 +35,15 @@ public class App extends Application {
     private final Thread thread = new Thread();
 
     private final ProgressBar progressBar = new ProgressBar();
+
     {
         progressBar.setVisible(false);
     }
 
-    MangaTable mangaTable;
-    ChaptersTable chaptersTable;
-    Client selectedClient;
+    private MangaTable mangaTable;
+    private ChaptersTable chaptersTable;
+    private Client selectedClient;
+    private TextField searchTextField;
 
     public static void main(String[] args) {
         config = new Config(SAVE_LOCATION, FILE_EXTENSION);
@@ -62,6 +67,7 @@ public class App extends Application {
         grid.getColumnConstraints().addAll(c1, c2);
         root.getChildren().add(grid);
 
+        //Clients table
         ClientsTable clientsTable = new ClientsTable(clientFactory.getRegisteredClients());
 
         clientsTable.setOnMouseClicked((event) -> {
@@ -72,6 +78,9 @@ public class App extends Application {
                     List<Manga> manga = client.getManga();
 
                     mangaTable.listMangas(manga);
+                    Platform.runLater(() -> {
+                        searchTextField.setDisable(false);
+                    });
                 });
                 newThread.start();
             }
@@ -79,8 +88,33 @@ public class App extends Application {
 
         grid.add(clientsTable, 0, 0);
 
+        //Search box
+        HBox searchMangaHBox = new HBox();
+        searchMangaHBox.setSpacing(10);
+        searchMangaHBox.setAlignment(Pos.CENTER_LEFT);
+
+        Label searchLabel = new Label(SEARCH_STRING);
+        searchTextField = new TextField();
+        searchTextField.setDisable(true);
+        searchTextField.addEventHandler(KeyEvent.KEY_RELEASED, (event -> {
+            String searchText = ((TextField) event.getSource()).getText();
+            mangaTable.setFilterCriteria(manga -> {
+                if (searchText == null || searchText.isEmpty()) {
+                    return true;
+                }
+
+                return manga.getMangaName().toLowerCase().startsWith(searchText.toLowerCase());
+            });
+        }));
+
+        searchMangaHBox.getChildren().addAll(searchLabel, searchTextField);
+
+        grid.add(searchMangaHBox, 0, 1);
+
+
+        //manga table
         mangaTable = new MangaTable();
-        grid.add(mangaTable, 0, 1);
+        grid.add(mangaTable, 0, 2);
 
         mangaTable.setOnMouseClicked((event) -> {
             if (event.getClickCount() == 2) {
@@ -92,18 +126,19 @@ public class App extends Application {
             }
         });
 
+        //Chapters table
         chaptersTable = new ChaptersTable();
-        grid.add(chaptersTable, 1, 0, 1, 2);
+        grid.add(chaptersTable, 1, 0, 1, 3);
 
         chaptersTable.setOnMouseClicked((event) -> {
             if (event.getClickCount() == 2) {
                 Consumer<Double> myConsumer = progress -> {
-                    if(progress!=-1){
+                    if (progress != -1) {
                         Platform.runLater(() -> {
                             progressBar.setProgress(progress);
                         });
                     }
-                    if(progress == 1){
+                    if (progress == 1) {
                         Platform.runLater(() -> {
                             progressBar.setVisible(false);
                         });
@@ -120,6 +155,7 @@ public class App extends Application {
             }
         });
 
+        //Save location
         Label label = new Label("Save Location:");
         TextField saveLocationTextBox = new TextField();
         saveLocationTextBox.setText(config.getSaveLocation());
@@ -140,9 +176,10 @@ public class App extends Application {
         HBox hBox = new HBox();
         hBox.getChildren().addAll(label, saveLocationTextBox, changeSaveDirectoryButton);
         hBox.setSpacing(10);
+        hBox.setAlignment(Pos.CENTER_LEFT);
 
-        grid.add(hBox, 0, 2);
-        grid.add(progressBar, 1, 2);
+        grid.add(hBox, 0, 3);
+        grid.add(progressBar, 1, 3);
 
         Scene scene = new Scene(root, 750, 500);
 
--- src/main/java/com/xhesiballa/crawler/ui/components/MangaTable.java
+++ src/main/java/com/xhesiballa/crawler/ui/components/MangaTable.java
@@ -1,18 +1,20 @@
 package com.xhesiballa.crawler.ui.components;
 
-import com.xhesiballa.crawler.interfaces.Client;
 import com.xhesiballa.crawler.model.Manga;
 import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.beans.value.ChangeListener;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.util.List;
+import java.util.function.Predicate;
 
 public class MangaTable extends TableView {
+    FilteredList<Manga> filteredMangasList;
 
     public MangaTable() {
         super();
@@ -25,18 +27,23 @@ public class MangaTable extends TableView {
 
         TableColumn<Manga, Number> indexColumn = new TableColumn<>("#");
         indexColumn.setSortable(false);
-        indexColumn.setCellValueFactory(column-> new ReadOnlyObjectWrapper<>(getItems().indexOf(column.getValue()) + 1));
+        indexColumn.setCellValueFactory(column -> new ReadOnlyObjectWrapper<>(getItems().indexOf(column.getValue()) + 1));
         indexColumn.prefWidthProperty().bind(widthProperty().multiply(0.1));
 
         getColumns().addAll(indexColumn, nameCol);
     }
 
-    public void listMangas(List<Manga> mangas){
+    public void listMangas(List<Manga> mangas) {
         ObservableList<Manga> mangaObservableList = FXCollections.observableArrayList(mangas);
-        setItems(mangaObservableList);
+        filteredMangasList = new FilteredList<>(mangaObservableList, p -> true);
+        setItems(filteredMangasList);
     }
 
-    public void setRowSelectListener(ChangeListener listener){
+    public void setRowSelectListener(ChangeListener listener) {
         getSelectionModel().selectedItemProperty().addListener(listener);
+    }
+
+    public void setFilterCriteria(Predicate<Manga> p) {
+        filteredMangasList.setPredicate(p);
     }
 }
